593b72e696fe8f29796e3065da1931884bf0bb09
Kenya Ishizuka <ken8116@icloud.com>
2021-03-20 20:16:06 +0900

    JWT実装

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,17 +1,26 @@
+require 'net/http'
+
 module Api
   module V1
     class BooksController < ApplicationController
+      MAX_PAGINATION_LIMIT = 100
+
+      before_action :authenticate_user, only: %i[create destroy]
+
       def index
-        books = Book.all
+        books = Book.limit(limit).offset(params[:offset])
+
         render json: BooksRepresenter.new(books).as_json
       end
 
       def create
-        #some logic
-        book = Book.new(book_params)
+        author = Author.create!(author_params)
+        book = Book.new(book_params.merge(author_id: author.id))
+
+        UpdateSkuJob.perform_later(book_params[:title])
 
         if book.save
-          render json: book, status: :created
+          render json: BookRepresenter.new(book).as_json, status: :created
         else
           render json: book.errors, status: :unprocessable_entity
         end
@@ -24,8 +33,22 @@ module Api
 
       private
 
+        def limit
+          [
+            params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
+            MAX_PAGINATION_LIMIT].min
+        end 
+
         def book_params
-          params.require(:book).permit(:title, :author)
+          params.require(:book).permit(:title)
+        end
+
+        def author_params
+          params.require(:author).permit(:first_name, :last_name, :age)
+        end
+
+        def authenticate_user
+          
         end
     end
   end
